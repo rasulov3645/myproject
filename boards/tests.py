3d285317ec1819663b1a39bc1c388d1bd9e73b58
Rasulov Gamzat <c0mrade3645@gmail.com>
2017-11-24 01:38:56 +0300

    rendering bootstrap forms

--- boards/tests.py
+++ boards/tests.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User
 from .views import new_topic
 from .models import Board, Topic, Post
 
+from .forms import NewTopicForm
 
 # Create your tests here.
 
@@ -101,10 +102,12 @@ class NewTopicTests(TestCase):
 		response = self.client.get(new_topic_url)
 		self.assertContains(response, 'href="{0}"'.format(board_topics_url))
 
+
 	def test_csrf(self):
 		url = reverse('new_topic', kwargs={'pk': 1})
 		response = self.client.get(url)
 		self.assertContains(response, 'csrfmiddlewaretoken')
+
 
 	def test_new_topic_valid_post_data(self):
 		url = reverse('new_topic', kwargs={'pk': 1})
@@ -116,14 +119,6 @@ class NewTopicTests(TestCase):
 		self.assertTrue(Topic.objects.exists())
 		self.assertTrue(Post.objects.exists())
 
-	def test_new_topic_invalid_post_data(self):
-		'''
-		Invalid post data should not redirect
-		The expected behavior is to show the form again with validation errors
-		'''
-		url = reverse('new_topic', kwargs={'pk': 1})
-		response = self.client.post(url, {})
-		self.assertEquals(response.status_code, 200)
 
 	def test_new_topic_invalid_post_data_empty_fields(self):
 		'''
@@ -140,3 +135,21 @@ class NewTopicTests(TestCase):
 		self.assertFalse(Topic.objects.exists())
 		self.assertFalse(Post.objects.exists())
 
+
+	def test_contains_form(self):  # <- new test
+		url = reverse('new_topic', kwargs={'pk': 1})
+		response = self.client.get(url)
+		form = response.context.get('form')
+		self.assertIsInstance(form, NewTopicForm)
+
+
+	def test_new_topic_invalid_post_data(self):  # <- updated this one
+		'''
+		Invalid post data should not redirect
+		The expected behavior is to show the form again with validation errors
+		'''
+		url = reverse('new_topic', kwargs={'pk': 1})
+		response = self.client.post(url, {})
+		form = response.context.get('form')
+		self.assertEquals(response.status_code, 200)
+		self.assertTrue(form.errors)
